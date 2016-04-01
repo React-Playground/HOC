578aa71dbc9bf81f80b843dffeee9855593923ae
darwin <oleg.umarov@gmail.com>
2016-04-01 17:16:05 -0500

    make auth HOC

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -8,6 +8,16 @@ export default function(ComposedComponent) {
       router: React.PropTypes.object
     }
 
+    componentWillMount() {
+      if(!this.props.authenticated)
+        this.context.router.push('/');
+    }
+
+    componentWillUpdate(nextProps) {
+      if(!nextProps.authenticated)
+        this.context.router.push('/')
+    }
+
     render() {
       return <ComposedComponent {...this.props} />
     }
