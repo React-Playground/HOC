aebfceca22268dc4923ceb7b89cc5e96894ef004
darwin <oleg.umarov@gmail.com>
2016-04-01 00:42:12 -0500

    add hidden component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+  <div className="">
+    <h1> Hidden Page </h1>
+    <ul>
+      <li>Hello</li>
+      <li>What is that</li>
+      <li>no way</li>
+    </ul>
+  </div>
+  );
+}
